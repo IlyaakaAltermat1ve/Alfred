81474ab86532efafebcd54322e7b4b0797f80a1f
bananaWeb <a89.volkov@gmail.com>
2018-01-29 18:55:34 +0300

    Первая загрузка

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿$(document).ready(function(){
+    var alfredSpeech ='Привет, гуманоиды';
+
+    $('.js-showHide').click(function() {
+        $('.alfred').toggleClass('-invisible');
+    })
+
+    $('.js-hello').click(function() {
+        $('.bubble').text(alfredSpeech)
+    });
+});
